9c2a69b85546d4a3b05bb52969996725326259a6
Christa Hatch <christahatch1994@pointloma.edu>
2019-02-19 14:29:32 -0800

    Create MusicEvent instance and set values

--- app/src/main/java/edu/miracostacollege/cs134/sandiegomusicevents/model/MusicEvent.java
+++ app/src/main/java/edu/miracostacollege/cs134/sandiegomusicevents/model/MusicEvent.java
@@ -20,4 +20,82 @@ public class MusicEvent {
     private String mCity;
     private String mState;
     private String mImageName;
+
+    public String getArtist() {
+        return mArtist;
+    }
+
+    public void setArtist(String artist) {
+        mArtist = artist;
+    }
+
+    public String getDate() {
+        return mDate;
+    }
+
+    public void setDate(String date) {
+        mDate = date;
+    }
+
+    public String getDay() {
+        return mDay;
+    }
+
+    public void setDay(String day) {
+        mDay = day;
+    }
+
+    public String getTime() {
+        return mTime;
+    }
+
+    public void setTime(String time) {
+        mTime = time;
+    }
+
+    public String getVenue() {
+        return mVenue;
+    }
+
+    public void setVenue(String venue) {
+        mVenue = venue;
+    }
+
+    public String getCity() {
+        return mCity;
+    }
+
+    public void setCity(String city) {
+        mCity = city;
+    }
+
+    public String getState() {
+        return mState;
+    }
+
+    public void setState(String state) {
+        mState = state;
+    }
+
+    public String getImageName() {
+        return mImageName;
+    }
+
+    public void setImageName(String imageName) {
+        mImageName = imageName;
+    }
+
+    @Override
+    public String toString() {
+        return "MusicEvent{" +
+                "mArtist='" + mArtist + '\'' +
+                ", mDate='" + mDate + '\'' +
+                ", mDay='" + mDay + '\'' +
+                ", mTime='" + mTime + '\'' +
+                ", mVenue='" + mVenue + '\'' +
+                ", mCity='" + mCity + '\'' +
+                ", mState='" + mState + '\'' +
+                ", mImageName='" + mImageName + '\'' +
+                '}';
+    }
 }
