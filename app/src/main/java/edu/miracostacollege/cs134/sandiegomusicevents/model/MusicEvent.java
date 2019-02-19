1759a5cf03b8398e0d2fed2501dad17233ac6ae5
Christa Hatch <christahatch1994@pointloma.edu>
2019-02-19 14:21:08 -0800

    Delete contents of MusicEvent class Add to MusicEven class

--- app/src/main/java/edu/miracostacollege/cs134/sandiegomusicevents/model/MusicEvent.java
+++ app/src/main/java/edu/miracostacollege/cs134/sandiegomusicevents/model/MusicEvent.java
@@ -7,53 +7,17 @@ package edu.miracostacollege.cs134.sandiegomusicevents.model;
  * about the event.
  *
  * @author Michael Paulding
- * @version 1.0
+ * @version 2.0
  */
 public class MusicEvent {
 
-    /**
-     * The titles of upcoming music events.
-     */
-    public static String[] titles = {
-            "Justin Timberlake", "Blues Traveler", "Gladys Knight", "Ariana Grande",
-            "Goo Goo Dolls", "Rebelution", "Carlos Santana", "Ozzy Osbourne"};
-
-
-    /**
-     * The details of upcoming music events.
-     */
-    public static String[] details = {
-            "February 21\n" +
-                    "Thursday - 8:00 PM\n" +
-                    "Pechanga Arena\n" +
-                    "San Diego, CA" ,
-            "February 28\n" +
-                    "Thursday - 7:00 PM\n" +
-                    "House of Blues\n" +
-                    "San Diego, CA" ,
-            "March 9\n" +
-                    "Saturday - 7:00 PM\n" +
-                    "Pechanga Resort and Casino\n" +
-                    "Temecula, CA",
-            "April 12\n" +
-                    "Friday - 3:30 AM\n" +
-                    "Coachella 2019\n" +
-                    "Indio, CA",
-            "June 14\n" +
-                    "Friday- 7:00 PM\n" +
-                    "North Island Amphitheatre\n" +
-                    "Chula Vista, CA",
-            "June 15\n" +
-                    "Saturday - 7:00 PM\n" +
-                    "North Island Amphitheatre\n" +
-                    "Chula Vista, CA",
-            "June 23\n" +
-                    "Sunday - 7:00 PM\n" +
-                    "North Island Amphitheatre\n" +
-                    "Chula Vista, CA",
-            "July 23\n" +
-                    "Tuesday - 7:30 PM\n" +
-                    "North Island Amphitheatre\n" +
-                    "Chula Vista, CA"
-    };
+    // Every instance variable in Java class should mimic the JSO file
+    private String mArtist;
+    private String mDate;
+    private String mDay;
+    private String mTime;
+    private String mVenue;
+    private String mCity;
+    private String mState;
+    private String mImageName;
 }
